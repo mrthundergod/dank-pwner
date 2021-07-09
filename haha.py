4583a4f25f5c677da0a18700080d3194a1954982
thundergod <themidnightlegendz@gmail.com>
2021-07-09 19:05:28 +0530

    Initial Commit

---
+++ haha.py
@@ -0,0 +1,22 @@
+import pyautogui as pg
+from time import sleep
+
+print("Let's Start Farming in 10 seconds")
+sleep(5)
+print("Farming")
+while True:
+	pg.typewrite('pls beg')
+	pg.press('enter')
+	pg.typewrite('pls hunt')
+	pg.press('enter')
+	pg.typewrite('pls dig')
+	pg.press('enter')
+	pg.typewrite('pls fish')
+	pg.press('enter')
+	pg.typewrite('pls pm')
+	pg.press('enter')
+	pg.typewrite('i')
+	pg.press('enter')
+	print("Get pwned Bitches!")
+	print("Gonna wait for 30secs")
+	sleep(30)
